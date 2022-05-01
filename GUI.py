6cb117daf6904d691032be4b62fbf75264955320
gimjh003 <46261152+gimjh003@users.noreply.github.com>
2022-05-01 15:44:11 +0900

    코드 가독성 증가를 위한 주석처리

--- GUI.py
+++ GUI.py
@@ -3,10 +3,11 @@ from tkinter import *
 import tkinter.messagebox as msgbox
 import webbrowser
 
+# 현재 표시된 정보
 current_content = ""
 current_view = []
 
-# 가져오기 후 추가
+# 공지사항 게시글 정보 수집 후 표시
 def announce_list():
     contents = info.get_announce()
     list_result.delete(0, END)
@@ -17,6 +18,7 @@ def announce_list():
     current_view = contents
     current_content = "announce"
 
+# 장학 게시글 정보 수집 후 표시
 def scholarship_list():
     contents = info.get_scholarship()
     list_result.delete(0, END)
@@ -27,12 +29,20 @@ def scholarship_list():
     current_view = contents
     current_content = "scholarship"
 
+# 학사일정 정보 수집 후 표시
 def schedule_list():
+    schedule_overall = info.get_schedule()
     list_result.delete(0, END)
+    checked_schedule = []
+    for schedule in schedule_overall:
+        if schedule in checked_schedule:
+            continue
+        list_result.insert(END, f"{schedule[0]} / {schedule[1]}")
+        checked_schedule.append(schedule)
     global current_content
     current_content = "schedule"
 
-# 무시 기능
+# 선택된 게시물 무시하기
 def ignore():
     global current_content
     if current_content == "schedule":
@@ -51,7 +61,7 @@ def ignore():
     except:
         msgbox.showinfo("알림", "무시할 게시물을 선택하세요.")
 
-# 선착순 추리기
+# 선착순 게시물만 추리기
 def limited():
     if current_content == "schedule":
         msgbox.showinfo("알림", "학사일정에서는 제공되지 않는 기능입니다.")
@@ -63,7 +73,7 @@ def limited():
         list_result.insert(END, content[0])
     current_view = limited_contents
 
-# 조회수 정렬
+# 조회수 높은 순서대로 정렬
 def sort_view():
     if current_content == "schedule":
         msgbox.showinfo("알림", "학사일정에서는 제공되지 않는 기능입니다.")
@@ -89,29 +99,17 @@ def open_url():
     except:
         msgbox.showinfo("알림","열람할 게시물을 선택하세요.")
 
-# 학사일정 확인
-def schedule_list():
-    schedule_overall = info.get_schedule()
-    list_result.delete(0, END)
-    checked_schedule = []
-    for schedule in schedule_overall:
-        if schedule in checked_schedule:
-            continue
-        list_result.insert(END, f"{schedule[0]} / {schedule[1]}")
-        checked_schedule.append(schedule)
-    global current_content
-    current_content = "schedule"
-
-# 기본형
+# GUI 기본 틀
 root = Tk()
 root.title("LMS-mini")
 root.geometry("600x750")
 root.resizable(False, False)
+root.iconbitmap("DJU-logo.ico")
 
 # 게시판 영역
 frame_board = LabelFrame(root, relief="solid", bd=1, text="정보")
 frame_board.pack(side="top", fill="x", padx=10, pady=10, ipady=3, ipadx=3)
-# 게시판-버튼 영역
+# 게시판-버튼
 btn_announce = Button(frame_board, text="공지사항", width=25, command=announce_list)
 btn_scholarship = Button(frame_board, text="장학", width=25, command=scholarship_list)
 btn_schedule = Button(frame_board, text="학사일정", width=25, command=schedule_list)
@@ -133,7 +131,7 @@ scrollbar.config(command=list_result.yview)
 # 옵션 영역
 frame_option = LabelFrame(root, relief="solid", bd=1, text="옵션")
 frame_option.pack(side="top", fill="x", padx=10, pady=10, ipady=3, ipadx=3)
-# 옵션-버튼 영역
+# 옵션-버튼
 btn_view = Button(frame_option, text="조회수", width=25, command=sort_view)
 btn_limited = Button(frame_option, text="선착순", width=25, command=limited)
 btn_ignore = Button(frame_option, text="무시", width=25, command=ignore)
@@ -141,10 +139,11 @@ btn_view.pack(side="left",padx=6)
 btn_limited.pack(side="left", padx=6)
 btn_ignore.pack(side="left", padx=6)
 
-# 링크 영역
+# 링크-버튼
 frame_link = Frame(root)
 frame_link.pack(fill="both", padx=10, pady=10, ipady=3, ipadx=3)
 btn_open = Button(frame_link, text="바로가기", command=open_url)
 btn_open.pack(fill="both")
 
+# 종료 방지 루프
 root.mainloop()
--- info.py
+++ info.py
@@ -7,7 +7,7 @@ announce_url = "https://www.dju.ac.kr/dju/na/ntt/selectNttList.do?mi=1188&bbsId=
 scholarship_url = "https://www.dju.ac.kr/dju/na/ntt/selectNttList.do?mi=3957&bbsId=1853"
 schedule_url = "https://www.dju.ac.kr/dju/sv/schdulView/schdulCalendarView.do?mi=1166"
 
-# 게시판 정보 가져오기 [제목, 게시일, 링크, 조회수]
+# 게시물 정보 가져오기 : [제목, 게시일, 링크, 조회수]
 def get_content(url):
     res = requests.get(url)
     res.raise_for_status()
@@ -27,23 +27,23 @@ def get_content(url):
         else: content_list.append([content_title, content_date, content_url, content_view])
     return content_list
 
-# 공지사항
+# 공지사항 게시물 정보 가져오기
 def get_announce():
     announce_list = get_content(announce_url)
     return announce_list
 
-# 장학
+# 장학 게시물 정보 가져오기
 def get_scholarship():
     scholarship_list = get_content(scholarship_url)
     return scholarship_list
 
-# 학사일정 [] = 전체, [날짜정보, 일정]
-schedule_res = requests.get(schedule_url)
-schedule_res.raise_for_status()
-schedule_html = BeautifulSoup(schedule_res.text, "lxml")
-schedule_contents = schedule_html.find("ul", attrs={"id":"schedule_month"}).find_all("div", attrs={"class":"schedule_calendar"})
-schedule_overall = []
+# 학사일정 정보 가져오기 : ["날짜", "정보"]
 def get_schedule():
+    schedule_res = requests.get(schedule_url)
+    schedule_res.raise_for_status()
+    schedule_html = BeautifulSoup(schedule_res.text, "lxml")
+    schedule_contents = schedule_html.find("ul", attrs={"id":"schedule_month"}).find_all("div", attrs={"class":"schedule_calendar"})
+    schedule_overall = []
     for schedule in schedule_contents:
         schedule_info = schedule.find_all("tbody")[1].find_all("tr")
         for schedule_detail in schedule_info:
@@ -52,15 +52,16 @@ def get_schedule():
             schedule_overall.append([schedule_date, schedule_label])
     return schedule_overall
 
-# 정렬
+# 조회수 내림차순 버블정렬
 def sort_by_view(arr):
     for i in range(len(arr)-1):
-        for j in range(len(arr)-1-i):
+        for j in range(len(arr)-i-1):
             if arr[j][3] < arr[j+1][3]:
                 arr[j], arr[j+1] = arr[j+1], arr[j]
     return arr
 
-# 선착순 확인
+# 게시물 본문에 '선착순' 단어가 들어있는지 확인
+# not_limited.txt : '선착순' 단어가 들어있지 않은 게시물을 따로 추려낸 것, 시간적 손실을 줄인다.
 def check_limited(arr):
     url = arr[2]
     res = requests.get(url)
@@ -79,7 +80,7 @@ def check_limited(arr):
             with open("not_limited.txt", "w", encoding="utf8") as file:
                 file.write(arr[0]+"\n")
 
-
+# 게시물 본문에 '선착순' 단어가 들어있는 게시물만을 분류한다.
 def collect_limited(arr):
     unregistered = []
     collected = []
@@ -102,8 +103,9 @@ def collect_limited(arr):
             collected.append(content)
     return collected
 
-# 무시하기
-def ignore(content): # content = [제목, 게시일, 링크, 조회수]
+# 선택한 게시물을 다음부터 무시한다.
+# ignore.txt : 게시물 정보를 가져오는 과정에서 해당 파일 내에 있는 제목에 해당하는 게시물을 무시하게 된다.
+def ignore(content):
     if os.path.isfile("ignore.txt"):
         title = [content[0]+"\n"]
         with open("ignore.txt", "r", encoding="utf8") as file:
@@ -115,6 +117,7 @@ def ignore(content): # content = [제목, 게시일, 링크, 조회수]
         with open("ignore.txt", "w", encoding="utf8") as file:
             file.write(content[0]+"\n")
 
+# 게시물의 제목을 매개변수로 받아 ignore.txt에 들어있는지 확인 후, 들어있다면 참을 반환한다.
 def check_ignore(title):
     if os.path.isfile("ignore.txt"):
         with open("ignore.txt", "r", encoding="utf8") as file:
@@ -122,5 +125,3 @@ def check_ignore(title):
             for line in lines:
                 if title in line:
                     return True
-                else: pass
-    else: pass
